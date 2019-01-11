ab6ee1af1714dc0cebd3c2c8e300fe731e16f2ff
Gingmer <826816058@qq.com>
2019-01-11 14:16:55 +0800

    90

--- springcloud-eureka-cluster/springcloud-eureka-cluster-client-customer/src/main/java/org/spring/springcloud/CustomerApplication.java
+++ springcloud-eureka-cluster/springcloud-eureka-cluster-client-customer/src/main/java/org/spring/springcloud/CustomerApplication.java
@@ -19,7 +19,7 @@ public class CustomerApplication {
     @Bean
     @LoadBalanced
     RestTemplate restTemplate() {
-        return new RestTemplate();  
+        return new RestTemplate();
     }
 
     public static void main(String[] args) {
